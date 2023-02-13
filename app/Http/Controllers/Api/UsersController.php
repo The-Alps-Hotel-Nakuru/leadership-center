0fa9a58dcee5849f42838506582440c2657de784
Steve Nyanumba <reach@stevenyanumba.com>
2023-02-14 00:03:27 +0300

    Login API Completed

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,28 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
 
 class UsersController extends Controller
 {
     public function login(Request $request)
     {
-        $this->validate($request, [
-            'email'=>'required|email',
-            'password'=>'required'
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
+
+        // return $validator;
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()]);
+        }
 
         $user = User::where('email', $request->email)->first();
 
-        if(!$user || !Hash::check($request->password, $user->password)){
-            throw ValidationException::withMessages([
-                'email'=>['The Provided Credentials are incorrect'],
-            ]);
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['errors' => ['email' => 'The Provided Credentials are incorrect'],]);
         }
 
 
-        return $user->createToken($request->email)->plainTextToken;
+        return response()->json([
+            'token'=>'Bearer '.$user->createToken($request->email)->plainTextToken
+        ]);
+
+        // return $request->all();
     }
 }
