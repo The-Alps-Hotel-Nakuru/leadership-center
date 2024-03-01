d26ed1fdd540690a49260a7ab20f67933bebc6f0
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-03-01 22:59:20 +0300

    Housing Levy not Deducted from February

--- app/Models/MonthlySalary.php
+++ app/Models/MonthlySalary.php
@@ -80,7 +80,7 @@ class MonthlySalary extends Model
     {
         $nssf = 0;
 
-        if (Carbon::parse($this->payroll->year . '-' . $this->payroll->month.'-01')->isBefore('2024-01-31')) {
+        if (Carbon::parse($this->payroll->year . '-' . $this->payroll->month . '-01')->isBefore('2024-01-31')) {
             if ($this->employee && $this->employee->isFullTimeBetween(Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->firstOfMonth(), Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->lastOfMonth())) {
                 $nssf = 200;
                 if ($this->gross_salary > (40000 / 12)) {
@@ -204,8 +204,12 @@ class MonthlySalary extends Model
     function getHousingLevyAttribute()
     {
         $levy = 0;
-        if ($this->employee && $this->employee->isFullTimeBetween(Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->firstOfMonth(), Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->lastOfMonth())) {
-            $levy = 0.015 * $this->gross_salary;
+        if (Carbon::parse($this->payroll->year . '-' . $this->payroll->month . '-01')->isBefore('2024-01-31')) {
+            if ($this->employee && $this->employee->isFullTimeBetween(Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->firstOfMonth(), Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->lastOfMonth())) {
+                $levy = 0.015 * $this->gross_salary;
+            }
+        } else {
+            $levy = 0;
         }
 
         return $levy;
