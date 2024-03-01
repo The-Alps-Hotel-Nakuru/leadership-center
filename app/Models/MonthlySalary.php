6b7769b8323acfa722c110c107e5ca8592f81c91
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-03-01 21:25:44 +0300

    New Feb 2024 NSSF Rates

--- app/Models/MonthlySalary.php
+++ app/Models/MonthlySalary.php
@@ -80,7 +80,7 @@ class MonthlySalary extends Model
     {
         $nssf = 0;
 
-        if (Carbon::parse($this->payroll->year . '-' . $this->payroll->month.'-01')->isBefore('2024-01-31')) {
+        if (Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->lessThan('2024-02')) {
             if ($this->employee && $this->employee->isFullTimeBetween(Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->firstOfMonth(), Carbon::parse($this->payroll->year . '-' . $this->payroll->month)->lastOfMonth())) {
                 $nssf = 200;
                 if ($this->gross_salary > (40000 / 12)) {
