98e95765b2fd6ddd35ca4eea74d003cc7153b505
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-05-06 01:50:33 +0300

    Corrected the is_covered attribute of the Holidays Module

--- resources/views/livewire/admin/payroll-payments/index.blade.php
+++ resources/views/livewire/admin/payroll-payments/index.blade.php
@@ -7,18 +7,20 @@
         <div class="row">
             @if ($payrolls)
                 @foreach ($payrolls as $payroll)
-                    <div class="col-2">
-                        <a href="{{ route('admin.payroll_payments.show', $payroll->id) }}" class="btn card">
-                            <div class="card-body">
-                                <h3 class="card-title">
-                                    {{ Carbon\Carbon::parse($payroll->year . '-' . $payroll->month)->format('F, Y') }}
-                                </h3>
-                                <p class="card-text">
-                                <h5><small>KES </small>{{ number_format($payroll->gross_payments_total) }}</h5>
-                                <p>
-                            </div>
-                        </a>
-                    </div>
+                    @if ($payroll->payments->count() > 0)
+                        <div class="col-2">
+                            <a href="{{ route('admin.payroll_payments.show', $payroll->id) }}" class="btn card">
+                                <div class="card-body">
+                                    <h3 class="card-title">
+                                        {{ Carbon\Carbon::parse($payroll->year . '-' . $payroll->month)->format('F, Y') }}
+                                    </h3>
+                                    <p class="card-text">
+                                    <h5><small>KES </small>{{ number_format($payroll->gross_payments_total) }}</h5>
+                                    <p>
+                                </div>
+                            </a>
+                        </div>
+                    @endif
                 @endforeach
             @endif
 
