a15265aa477e46370933b43135fd20a0729bd1b2
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-05-06 00:43:48 +0300

    Added the Holidays Table

--- app/Http/Livewire/Admin/Holidays/Index.php
+++ app/Http/Livewire/Admin/Holidays/Index.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Admin\Holidays;
 
+use App\Models\Holiday;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+
     public function render()
     {
-        return view('livewire.admin.holidays.index');
+        return view('livewire.admin.holidays.index', [
+            'holidays' => Holiday::orderBy('created_at', 'desc')->paginate(10),
+        ]);
     }
 }
--- resources/views/livewire/admin/holidays/index.blade.php
+++ resources/views/livewire/admin/holidays/index.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <x-slot:header>Holidays</x-slot:header>
+
+    <div class="card">
+        <div class="card-header">
+            <h5>List of Holidays Added</h5>
+        </div>
+        <table class="table table-hover  table-responsive card-body">
+            <thead class="">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($holidays as $holiday)
+                    <tr>
+                        <td scope="row">{{ $holiday->id }}</td>
+                        <td>{{ $holiday->name }}</td>
+                        <td>{{ Carbon\Carbon::parse($holiday->date)->format('jS F, Y') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
