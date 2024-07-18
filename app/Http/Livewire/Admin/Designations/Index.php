207a06c19e7efc91e3d5c779e6d304cf91601b21
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-07-18 14:03:20 +0300

    Added the Delete Function into Designations

--- app/Http/Livewire/Admin/Designations/Index.php
+++ app/Http/Livewire/Admin/Designations/Index.php
@@ -10,11 +10,26 @@ class Index extends Component
 {
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
-    
+
+    public function delete($id)
+    {
+        $designation = Designation::find($id);
+
+        if (!$designation->employees->count() > 0) {
+            $designation->delete();
+            $this->emit('done', [
+                'success' => 'Successfully Deleted the designation'
+            ]);
+        } else {
+            $this->emit('done', [
+                'danger' => 'There are Employees Assigned to this designation'
+            ]);
+        }
+    }
     public function render()
     {
         return view('livewire.admin.designations.index', [
-            'designations'=>Designation::paginate(10)
+            'designations' => Designation::paginate(10)
         ]);
     }
 }
