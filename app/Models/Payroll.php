2e7bded3e9d9999ff1d23975e002dc9efc8c6d0b
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-08-01 22:59:49 +0300

    The Payroll Formula Edited

--- app/Models/Payroll.php
+++ app/Models/Payroll.php
@@ -50,7 +50,7 @@ class Payroll extends Model
         $amount = 0;
         if (count($this->monthlySalaries) > 0) {
             foreach ($this->monthlySalaries as $salary) {
-                if ($salary->employee && $salary->employee->ActiveContractDuring($this->year . '-' . $this->month)->is_casual()) {
+                if ($salary->employee && $salary->employee->ActiveContractDuring($this->year . '-' . $this->month)->is_casual) {
                     $amount += $salary->gross_salary;
                 }
             }
@@ -63,7 +63,7 @@ class Payroll extends Model
         $amount = 0;
         if (count($this->monthlySalaries) > 0) {
             foreach ($this->monthlySalaries as $salary) {
-                if ($salary->employee && $salary->employee->ActiveContractDuring($this->year . '-' . $this->month)->is_casual()) {
+                if ($salary->employee && $salary->employee->isCasual($this->year . '-' . $this->month)->is_casual()) {
                     $amount += $salary->net_pay;
                 }
             }
