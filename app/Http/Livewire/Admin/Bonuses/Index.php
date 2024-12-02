edc111b2bf068a7e9076ca859285d8148362226c
Steve Nyanumba <reach@stevennmb9@gmail.com>
2024-12-02 15:34:38 +0300

    Bonuses Delete Function

--- app/Http/Livewire/Admin/Bonuses/Index.php
+++ app/Http/Livewire/Admin/Bonuses/Index.php
@@ -7,6 +7,7 @@ use App\Exports\BonusesTemplateExport;
 use App\Exports\FinesDataExport;
 use App\Models\Bonus;
 use App\Models\Log;
+use App\Models\Payroll;
 use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -23,20 +24,30 @@ class Index extends Component
         'done' => 'render'
     ];
 
-    // public function delete($id)
-    // {
-    //     $bonus  = Bonus::find($id);
-    //     $bonus->delete();
+    public function delete($id)
+    {
+        try {
+            $bonus  = Bonus::find($id);
+            if (Payroll::where('month', $bonus->month)->where('year', $bonus->year)->first()->is_paid) {
+                throw new \Exception("Payroll attached to this bonus is already paid", 1);
+            }
+            $bonus->delete();
 
-    //     $log = new Log();
-    //     $log->user_id = auth()->user()->id;
-    //     $log->model = 'App\Models\Bonus';
-    //     $log->payload = "<strong>" . auth()->user()->name . "</strong> has Deleted Bonus for <strong>" . $bonus->employee->user->name . ' on ' . Carbon::parse($bonus->created_at)->format('j F, Y - h:i A') . "</strong> amounting to <strong>KES " . number_format($bonus->amount_kes) . "</strong> in the system";
+            $log = new Log();
+            $log->user_id = auth()->user()->id;
+            $log->model = 'App\Models\Bonus';
+            $log->payload = "<strong>" . auth()->user()->name . "</strong> has Deleted Bonus for <strong>" . $bonus->employee->user->name . ' on ' . Carbon::parse($bonus->created_at)->format('j F, Y - h:i A') . "</strong> amounting to <strong>KES " . number_format($bonus->amount_kes) . "</strong> in the system";
 
-    //     $this->emit('done', [
-    //         'success' => "Successfully Deleted the Bonus from the system"
-    //     ]);
-    // }
+            $this->emit('done', [
+                'success' => "Successfully Deleted the Bonus from the system"
+            ]);
+        } catch (\Throwable $th) {
+            $this->emit('done', [
+                'error' => "Something went wrong: " . $th->getMessage()
+            ]);
+            //throw $th;
+        }
+    }
 
     // function downloadBonusesData()
     // {
