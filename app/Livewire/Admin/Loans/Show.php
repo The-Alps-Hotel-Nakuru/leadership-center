c4dae56e2f5f7d313a906945d00f08416a12b289
Steve Nyanumba <reach@stevennmb9@gmail.com>
2025-02-03 19:20:30 +0300

    Loan Deduction deletion

--- app/Livewire/Admin/Loans/Show.php
+++ app/Livewire/Admin/Loans/Show.php
@@ -18,7 +18,7 @@ class Show extends Component
     public function delete($id)
     {
         $deduction = LoanDeduction::find($id);
-        if (!$deduction->loan->hasBeganSettlement()) {
+        if (!$deduction->loan->hasBeganSettlement() && $deduction->is_settled) {
             $deduction->delete();
             $this->dispatch(
                 'done',
